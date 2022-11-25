b9795dfc4295b0f31146e640115f8b50577bc5b0
Ayush Chadha <elegantShock3307@gmail.com>
2022-11-26 00:05:51 +0530

    basic mechanics done

---
+++ index.js
@@ -0,0 +1,38 @@
+// add wrong word highlight
+// add completion screen
+// add stats
+
+var randomWords = require('random-words')
+let textPara = document.getElementsByClassName("text")[0]
+let DummyWords = 15 
+let text = "";
+let count = 0
+function buildDummyPara(){
+    text = randomWords({ exactly: DummyWords, join: ' '/*,formatter: (word) => `<span id=\"${word}\">` + word + "</span>"}*/})
+    textPara.innerHTML = text
+}
+
+function keyPress(){
+    document.addEventListener("keydown", ((e)=>{
+        console.log(String.fromCharCode(e.keyCode || e.which).toLocaleLowerCase(), textPara.innerText.charAt(count))
+        if(String.fromCharCode(e.keyCode || e.which).toLocaleLowerCase() == textPara.innerText.charAt(count)) highlightText(count+1)
+        else console.log("key code")
+        count++
+    }))
+}
+function highlightText(index){
+    // index+=countSpaces(textPara.innerText.slice(0,index))
+    textPara.innerHTML = "<span id=\"highlight\">"+ textPara.innerText.slice(0,index) + "</span>" + textPara.innerText.slice(index,textPara.innerText.length)
+}
+
+function countSpaces(str){
+    let count = 0;
+    str.split("").forEach(element => {
+        if(element === " ") count++;
+    });
+    return count;
+}
+
+
+// buildDummyPara()
+keyPress();
